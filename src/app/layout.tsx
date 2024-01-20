a5ef75ec763cb00d59ae6b6c71452dd5e827d124
elijahnikov <enkv@proton.me>
2024-01-20 22:18:15 +0000

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={GeistMono.className}>
         {children}
-        {/* <Analytics /> */}
+        <Analytics />
       </body>
       <Toaster />
     </html>
